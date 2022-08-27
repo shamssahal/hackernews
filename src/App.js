c8f4756ba9634d89187c92d5fdd8798eb2911b29
Sahal Shams <shamssahal@gmail.com>
2022-08-28 03:40:24 +0530

    Init: Project Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+import Home from "./Components/Home";
+
+const App = () => {
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "/assets/js/app.js";
+        script.async = true;
+        document.body.appendChild(script);
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+            </Routes>
+        </Router>
+    );
+};
+
+
+
 
 export default App;
